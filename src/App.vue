60e23ce1c3775764742968bb2528c1e4ff58e3ca
Danilo-GB <rubendanilo.garcia@gmail.com>
2022-01-15 17:26:53 -0500

    Tailwind added

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap");
+:root {
+  --main-blue: #030c18;
+  --complementary-blue: #072142;
+  --main-red: #ff4d5a;
+}
+* {
+  font-family: "Oswald", sans-serif;
+}
+</style>
