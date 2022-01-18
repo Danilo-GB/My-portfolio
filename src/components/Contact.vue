916a84bd79ac87475982cf47ed686ba47814946a
Danilo-GB <rubendanilo.garcia@gmail.com>
2022-01-18 18:05:17 -0500

    Form update

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -12,21 +12,25 @@
           class="text-black text-md"
           netlify
           netlify-honeypot="bot-field"
+          @submit.prevent="handleSubmit"
         >
           <input type="text" name="bot-field" hidden />
           <input
+            v-model="form.name"
             type="text"
             name="name"
             placeholder="Enter Your Name"
             class="w-full md:w-1/3 h-16 px-4 py-2 block my-10 rounded-lg"
           />
           <input
+            v-model="form.email"
             type="email"
             name="email"
             placeholder="Enter Your Email"
             class="w-full md:w-1/2 px-4 py-2 h-16 block my-10 rounded-lg"
           />
           <input
+            v-model="form.message"
             type="textarea"
             name="message"
             placeholder="Enter Your message"
@@ -36,10 +40,54 @@
             type="submit"
             class="contact-button text-white cursor-pointer"
           >
-            SEND
+            {{ formStatus }}
           </button>
         </form>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formStatus: "SEND",
+      form: {
+        name: "",
+        email: "",
+        message: "",
+      },
+    };
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          (key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join("&");
+    },
+    handleSubmit() {
+      fetch("/", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/x-www-urlenccoded",
+        },
+        body: this.encode({
+          "form-name": "contact",
+          ...this.form,
+        }),
+      })
+        .then(() => this.formSent)
+        .catch((e) => (this.formStatus = "Oops, error"));
+    },
+    formSent() {
+      this.formStatus = "SENT";
+      this.form.name = "";
+      this.form.email = "";
+      this.form.message = "";
+    },
+  },
+};
+</script>
